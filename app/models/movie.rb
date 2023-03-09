b45f2c5699cac6c882791eb2e7b4f9ce8eb0d2fa
Ozair Ismail <ojismail@gmail.com>
2023-03-09 14:09:41 +0000

    Third commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,16 @@
 class Movie < ApplicationRecord
+  before_destroy :check_for_bookmarks
+
   has_many :bookmarks, dependent: :destroy
 
   validates :title, :overview, presence: true
   validates :title, uniqueness: { scope: :overview }
 
-  before_destroy :check_for_bookmarks
-
   private
 
   def check_for_bookmarks
     if bookmarks.any?
-      errors.add(:base, 'Cannot delete movie with associated bookmarks')
-      throw :abort
+      raise ActiveRecord::InvalidForeignKey.new('Cannot delete movie with associated bookmarks')
     end
   end
 end
