6bd656f8b4020b23f5e4daef1c3796e4102596de
Ozair Ismail <ojismail@gmail.com>
2023-03-09 13:54:45 +0000

    Second commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+
+  validates :title, :overview, presence: true
+  validates :title, uniqueness: { scope: :overview }
+
+  before_destroy :check_for_bookmarks
+
+  private
+
+  def check_for_bookmarks
+    if bookmarks.any?
+      errors.add(:base, 'Cannot delete movie with associated bookmarks')
+      throw :abort
+    end
+  end
 end
