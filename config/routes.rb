48d8e104b561339a96cbfa3cd261ee19e1852fe5
Ozair Ismail <ojismail@gmail.com>
2023-03-10 10:30:32 +0000

    Fourth commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :movies do
+    resources :bookmarks, only: [:new, :create]
+  end
 
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:create]
+  end
+
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
